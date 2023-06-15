67877e3f1fea9be31d31964f2874f53bd6add57b
Andrei Nups <nupriyonok@gmail.com>
2023-06-15 20:51:29 +0200

    first try to add search

--- src/main/java/org/olegmell/controller/AddressController.java
+++ src/main/java/org/olegmell/controller/AddressController.java
@@ -1,10 +1,8 @@
 package org.olegmell.controller;
 
-import org.olegmell.domain.Address;
 import org.olegmell.domain.AddressItem;
 import org.olegmell.service.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
